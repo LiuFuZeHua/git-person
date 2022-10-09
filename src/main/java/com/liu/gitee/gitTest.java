edc85cf036328932f93b176a2473095c4cdd4622
Liufu <Liufu@123.com>
2022-10-09 16:54:29 +0800

    Initial commit

---
+++ src/main/java/com/liu/gitee/gitTest.java
@@ -0,0 +1,9 @@
+package com.liu.gitee;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,gitee1");
+        System.out.println("hello,gitee2");
+        System.out.println("hello,gitee3");
+    }
+}
